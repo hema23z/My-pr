8a3305c74cfe7031a9e5ca015f0d785365668464
Your Name <you@example.com>
2024-12-02 11:15:25 +0200

    first commit

---
+++ Assets/MultiplicationTable.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class NewEmptyCSharpScript : MonoBehaviour
+
+ 
+{
+
+       void Start()
+    {
+           for (int x =1; x <= 10; x++){
+            int sum = Multiply(5, x);
+            Debug.Log(sum);
+           }
+           
+    }
+    int  Multiply(int num1 , int num2 )
+{
+    int Multiplication=num1*num2;
+    return Multiplication;
+
+    }
+}
